c9d727f37345c517c267bdd26bf6380decd1fd77
siopao2001 <sio_pao2001@yahoo.ca>
2021-08-19 02:17:43 +0000

    Second Commit

--- fetcher.js
+++ fetcher.js
@@ -1,21 +1,21 @@
 const fs = require('fs')
 const request = require('request')
-
 let variables = process.argv.slice(2)
 let url = variables[0]
 let fileName = variables[1]
-
 request(url, (error, response, body) => {
-   console.log('error:', error);
-    fs.writeFile(fileName, body, (err) => {
-     if (err) {
-    console.log(err)
-} else {
-   fs.stat(fileName, (err, stats) => {
-      console.log(`Downloaded and saved ${stats.size} bytes to ${fileName}`)
-   })
-  }
- })
+  if (error) {
+    console.log('page cannot be reached');
+  };
+  fs.writeFile(fileName, body, (err) => {
+    if (err) {
+      console.log('File path is invalid')
+    } else {
+      fs.stat(fileName, (err, stats) => {
+        console.log(`Downloaded and saved ${stats.size} bytes to ${fileName}`)
+      })
+    }
+  })
 });
 
 
